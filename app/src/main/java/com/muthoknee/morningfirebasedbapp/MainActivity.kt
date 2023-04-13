9231f8908b4b7a12eb22c2ed06e93af38afb7a09
mutho-knee <mwangimichelle911@gmail.com>
2023-04-13 10:38:05 +0300

    initial commit

---
+++ app/src/main/java/com/muthoknee/morningfirebasedbapp/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.muthoknee.morningfirebasedbapp
+
+import android.app.ProgressDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+    lateinit var edtName:EditText
+    lateinit var edtEmail:EditText
+    lateinit var edtIdNumber:EditText
+    lateinit var btnSave: Button
+    lateinit var btnView:Button
+    lateinit var progressDialog: ProgressDialog
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        edtName = findViewById(R.id.mEdtName)
+        edtEmail = findViewById(R.id.mEdtEmail)
+        edtIdNumber = findViewById(R.id.mEdtIdNumber)
+        btnSave = findViewById(R.id.mBtnSave)
+        btnView = findViewById(R.id.mBtnView)
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Saving")
+        progressDialog.setMessage("Please wait...")
+        btnSave.setOnClickListener{
+            var name = edtName.text.toString().trim()
+            var email = edtEmail.text.toString().trim()
+            var idNumber = edtIdNumber.text.toString().trim()
+            var id = System.currentTimeMillis().toString()
+            if (name.isEmpty()){
+                edtName.setError("Please fill this field")
+                edtName.requestFocus()
+            }else if (email.isEmpty()){
+                edtEmail.setError("Please fill this field")
+                edtEmail.requestFocus()
+            }else if (idNumber.isEmpty()){
+                edtIdNumber.setError("Please fill this field")
+                edtIdNumber.requestFocus()
+            }else{
+                //Proceed to save
+                //Prepare the user to be saved
+                var user = User(name, email, idNumber, id)
+                //Create a reference in the firebase database
+                var ref = FirebaseDatabase.getInstance().getReference().child("Users/"+id)
+
+                //Show the progress to start saving
+                progressDialog.show()
+                ref.setValue(user).addOnCompleteListener{
+                    //Dismiss the progress and check if the task was successful
+                    task ->
+                    progressDialog.dismiss()
+                    if (task.isSuccessful){
+                        Toast.makeText(this,"User saved successfully", Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(this, "User saving failed", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+        btnView.setOnClickListener{
+
+        }
+    }
+}
---
+++ app/src/main/java/com/muthoknee/morningfirebasedbapp/User.kt
@@ -0,0 +1,18 @@
+package com.muthoknee.morningfirebasedbapp
+
+class User {
+    var name:String = ""
+    var email:String = ""
+    var idNumber:String = ""
+    var id:String = ""
+
+    constructor(name: String, email: String, idNumber: String, id: String) {
+        this.name = name
+        this.email = email
+        this.idNumber = idNumber
+        this.id = id
+    }
+
+    constructor()
+
+}
