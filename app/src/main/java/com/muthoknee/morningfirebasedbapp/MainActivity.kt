2914bf2fc7084e186d2c33a7385bb2121ba2f8ad
mutho-knee <mwangimichelle911@gmail.com>
2023-04-19 10:23:53 +0300

    fourth commit

--- app/src/main/java/com/muthoknee/morningfirebasedbapp/MainActivity.kt
+++ app/src/main/java/com/muthoknee/morningfirebasedbapp/MainActivity.kt
@@ -4,8 +4,11 @@ import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
 import com.google.firebase.database.FirebaseDatabase
 
@@ -15,7 +18,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var edtIdNumber:EditText
     lateinit var btnSave: Button
     lateinit var btnView:Button
+    lateinit var spinnerOptions: Spinner
     lateinit var progressDialog: ProgressDialog
+    lateinit var optionSelected:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,9 +29,24 @@ class MainActivity : AppCompatActivity() {
         edtIdNumber = findViewById(R.id.mEdtIdNumber)
         btnSave = findViewById(R.id.mBtnSave)
         btnView = findViewById(R.id.mBtnView)
+        spinnerOptions = findViewById(R.id.mSpinnerOptions)
         progressDialog = ProgressDialog(this)
         progressDialog.setTitle("Saving")
         progressDialog.setMessage("Please wait...")
+        spinnerOptions.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                optionSelected = parent?.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
         btnSave.setOnClickListener{
             var name = edtName.text.toString().trim()
             var email = edtEmail.text.toString().trim()
@@ -44,7 +64,7 @@ class MainActivity : AppCompatActivity() {
             }else{
                 //Proceed to save
                 //Prepare the user to be saved
-                var user = User(name, email, idNumber, id)
+                var user = User(name, email, idNumber, id, optionSelected)
                 //Create a reference in the firebase database
                 var ref = FirebaseDatabase.getInstance().getReference().child("Users/"+id)
 
--- app/src/main/java/com/muthoknee/morningfirebasedbapp/User.kt
+++ app/src/main/java/com/muthoknee/morningfirebasedbapp/User.kt
@@ -5,12 +5,14 @@ class User {
     var email:String = ""
     var idNumber:String = ""
     var id:String = ""
+    var itemSelected:String = ""
 
-    constructor(name: String, email: String, idNumber: String, id: String) {
+    constructor(name: String, email: String, idNumber: String, id: String, itemSelected: String) {
         this.name = name
         this.email = email
         this.idNumber = idNumber
         this.id = id
+        this.itemSelected = itemSelected
     }
 
     constructor()
